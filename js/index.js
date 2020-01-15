730000ba06e0150838aad47871fab35875290ae2
Kevin Jensen <jensenk28@yahoo.com>
2020-01-14 22:53:46 -0800

    added splash page

--- js/index.js
+++ js/index.js
@@ -1,3 +1,31 @@
+var splash = document.querySelector('.splash');
+
+splash.addEventListener('click', () => {
+    splash.style.display = 'none';
+})
+
+splash.addEventListener('touchend', () => {
+    splash.style.display = 'none';
+})
+
+splash.addEventListener('scroll', () => {
+    splash.style.display = 'none';
+})
+
+splash.addEventListener('touchmove', () => {
+    splash.style.display = 'none';
+})
+
+// var splash = document.querySelector('');
+
+// splash.addEventListener('animationend', () => {
+//     splash.style.display = 'none';
+// })
+
+// navigator.splashscreen.hide()
+
+
+
 // const backgroundImg = document.querySelector('.hero-img');
 
 // backgroundImg.style.backgroundImage = '.hero-img';
