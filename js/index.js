f328a4b51c98414b1ff3f9358c55ba263d7e3d5c
Kevin Jensen <jensenk28@yahoo.com>
2020-01-14 18:40:37 -0800

    starting over

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const headerText = document.querySelector('header');
+
+headerText.addEventListener('mouseover', () => {
+    headerText.style.backgroundColor = 'blue';
+})
+
+headerText.addEventListener('mouseout', () => {
+    headerText.style.backgroundColor = 'white';
+})
